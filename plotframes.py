ffaf3c90c7ad665dc6774063cf41888f634931df
Stephen Bailey <stephen.code@gmail.com>
2018-08-26 16:30:13 -0700

    WIP emission/absorption lines

--- plotframes.py
+++ plotframes.py
@@ -1,8 +1,8 @@
 import numpy as np
 import bokeh.plotting as bk
 
-from bokeh.models import ColumnDataSource, CustomJS
-from bokeh.models.widgets import Slider, Button, Div
+from bokeh.models import ColumnDataSource, CustomJS, LabelSet, Label, Span
+from bokeh.models.widgets import Slider, Button, Div, CheckboxButtonGroup
 from bokeh.layouts import widgetbox
 # from bokeh.layouts import row, column
 
@@ -63,8 +63,14 @@ def plotframes(framefiles, notebook=False):
     for spec in cds_spectra:
         fig.line('plotwave', 'plotflux', source=spec, line_color=colors[spec.name])
 
+    #-----
+    #- Emission and absorption lines
+    line_data, lines, line_labels = add_lines(fig, z=0)
+
+    #-----
+    #- Add widgets for controling plots
     zslider_callback  = CustomJS(
-        args=dict(spectra=cds_spectra),
+        args=dict(spectra=cds_spectra, line_data=line_data, line_lables=line_labels),
         code="""
         var z = cb_obj.value
         for (var i=0; i<spectra.length; i++) {
@@ -76,14 +82,48 @@ def plotframes(framefiles, notebook=False):
             }
             spectra[i].change.emit()
         }
+
+        // TODO: add rest/obs wave button; adjust line locations
+        // var restwave = line_data.data['restwave']
+        // var plotwave = line_data.data['plotwave']
+        // for (var i=0; i<restwave.length; i++) {
+        //     plotwave[i] = restwave[i]*(1+z)
+        // }
         """)
 
-    zslider = Slider(start=0, end=2.0, value=0.0, step=0.01, title='Redshift')
+    zslider = Slider(start=0, end=4.0, value=0.0, step=0.01, title='Redshift')
     zslider.js_on_change('value', zslider_callback)
 
     ifiberslider = Slider(start=0, end=fr.nspec-1, value=0, step=1, title='Fiber')
     smootherslider = Slider(start=1, end=21, value=1, step=1, title='Smooth')
     target_info = Div(text=target_names[0])
+
+    #-----
+    #- Toggle lines
+    lines_button_group = CheckboxButtonGroup(
+            labels=["Emission", "Absorption"], active=[0, 0])
+
+    lines_callback = CustomJS(
+        args = dict(line_data=line_data, lines=lines, line_labels=line_labels),
+        code="""
+        var show_emission = cb_obj.value[0]
+        var show_absorption = cb_obj.value[1]
+        var emission = line_data['emission']
+        console.log('blat')
+        for(var i=0; i<lines.length; i++) {
+            if(show_emission && emission[i]) {
+                lines[i].display = True
+                console.log('yes'+i)
+            } else {
+                lines[i].display = False
+                console.log('no'+i)
+            }
+        }
+        line_data.change.emit()
+        """
+    )
+    lines_button_group.js_on_event('button_click', lines_callback)
+    # lines_button_group.js_on_change('value', lines_callback)
 
     #-----
     update_plot = CustomJS(
@@ -92,6 +132,7 @@ def plotframes(framefiles, notebook=False):
             fibermap = cds_fibermap,
             ifiberslider = ifiberslider,
             smootherslider = smootherslider,
+            lines_button_group = lines_button_group,
             target_info = target_info,
             ),
         code = """
@@ -154,6 +195,7 @@ def plotframes(framefiles, notebook=False):
         navigator,
         widgetbox(smootherslider),
         widgetbox(zslider),
+        widgetbox(lines_button_group),
         ))
 
     #--- DEBUG ---
@@ -161,11 +203,117 @@ def plotframes(framefiles, notebook=False):
     # IPython.embed()
     #--- DEBUG ---
 
+_line_list = [
+    #
+    # This is the set of emission lines from the spZline files.
+    # See $IDLSPEC2D_DIR/etc/emlines.par
+    # Wavelengths are in air for lambda > 2000, vacuum for lambda < 2000.
+    # TODO: convert to vacuum wavelengths
+    #
+    {"name" : "Lyα",      "longname" : "Lyman α",        "lambda" : 1215.67,  "emission": True },
+    {"name" : "N V",      "longname" : "N V 1240",       "lambda" : 1240.81,  "emission": True },
+    {"name" : "C IV",     "longname" : "C IV 1549",      "lambda" : 1549.48,  "emission": True },
+    {"name" : "He II",    "longname" : "He II 1640",     "lambda" : 1640.42,  "emission": True },
+    {"name" : "C III]",   "longname" : "C III] 1908",    "lambda" : 1908.734, "emission": True },
+    {"name" : "Mg II",    "longname" : "Mg II 2799",     "lambda" : 2799.49,  "emission": True },
+    {"name" : "[O II]",   "longname" : "[O II] 3725",    "lambda" : 3726.032, "emission": True },
+    {"name" : "[O II]",   "longname" : "[O II] 3727",    "lambda" : 3728.815, "emission": True },
+    {"name" : "[Ne III]", "longname" : "[Ne III] 3868",  "lambda" : 3868.76,  "emission": True },
+    {"name" : "Hζ",       "longname" : "Balmer ζ",       "lambda" : 3889.049, "emission": True },
+    {"name" : "[Ne III]", "longname" : "[Ne III] 3970",  "lambda" : 3970.00,  "emission": True },
+    {"name" : "Hε",       "longname" : "Balmer ε",       "lambda" : 3970.072, "emission": True },
+    {"name" : "Hδ",       "longname" : "Balmer δ",       "lambda" : 4101.734, "emission": True },
+    {"name" : "Hγ",       "longname" : "Balmer γ",       "lambda" : 4340.464, "emission": True },
+    {"name" : "[O III]",  "longname" : "[O III] 4363",   "lambda" : 4363.209, "emission": True },
+    {"name" : "He II",    "longname" : "He II 4685",     "lambda" : 4685.68,  "emission": True },
+    {"name" : "Hβ",       "longname" : "Balmer β",       "lambda" : 4861.325, "emission": True },
+    {"name" : "[O III]",  "longname" : "[O III] 4959",   "lambda" : 4958.911, "emission": True },
+    {"name" : "[O III]",  "longname" : "[O III] 5007",   "lambda" : 5006.843, "emission": True },
+    {"name" : "He II",    "longname" : "He II 5411",     "lambda" : 5411.52,  "emission": True },
+    {"name" : "[O I]",    "longname" : "[O I] 5577",     "lambda" : 5577.339, "emission": True },
+    {"name" : "[N II]",   "longname" : "[N II] 5755",    "lambda" : 5754.59,  "emission": True },
+    {"name" : "He I",     "longname" : "He I 5876",      "lambda" : 5875.68,  "emission": True },
+    {"name" : "[O I]",    "longname" : "[O I] 6300",     "lambda" : 6300.304, "emission": True },
+    {"name" : "[S III]",  "longname" : "[S III] 6312",   "lambda" : 6312.06,  "emission": True },
+    {"name" : "[O I]",    "longname" : "[O I] 6363",     "lambda" : 6363.776, "emission": True },
+    {"name" : "[N II]",   "longname" : "[N II] 6548",    "lambda" : 6548.05,  "emission": True },
+    {"name" : "Hα",       "longname" : "Balmer α",       "lambda" : 6562.801, "emission": True },
+    {"name" : "[N II]",   "longname" : "[N II] 6583",    "lambda" : 6583.45,  "emission": True },
+    {"name" : "[S II]",   "longname" : "[S II] 6716",    "lambda" : 6716.44,  "emission": True },
+    {"name" : "[S II]",   "longname" : "[S II] 6730",    "lambda" : 6730.82,  "emission": True },
+    {"name" : "[Ar III]", "longname" : "[Ar III] 7135",  "lambda" : 7135.790, "emission": True },
+    #
+    # Absorption lines
+    #
+    # {"name" : "Hζ",   "longname" : "Balmer ζ",         "lambda" : 3889.049, "emission": False },
+    {"name" : "K",    "longname" : "K (Ca II 3933)",   "lambda" : 3933.7,   "emission": False },
+    {"name" : "H",    "longname" : "H (Ca II 3968)",   "lambda" : 3968.5,   "emission": False },
+    # {"name" : "Hε",   "longname" : "Balmer ε",         "lambda" : 3970.072, "emission": False },
+    # {"name" : "Hδ",   "longname" : "Balmer δ",         "lambda" : 4101.734, "emission": False },
+    {"name" : "G",    "longname" : "G (Ca I 4307)",    "lambda" : 4307.74,  "emission": False },
+    {"name" : "Hγ",   "longname" : "Balmer γ",         "lambda" : 4340.464, "emission": False },
+    {"name" : "Hβ",   "longname" : "Balmer β",         "lambda" : 4861.325, "emission": False },
+    {"name" : "Mg I", "longname" : "Mg I 5175",        "lambda" : 5175.0,   "emission": False },
+    {"name" : "D2",   "longname" : "D2 (Na I 5889)",   "lambda" : 5889.95,  "emission": False },
+    # {"name" : "D",    "longname" : "D (Na I doublet)", "lambda": 5892.9,   "emission": False },
+    {"name" : "D1",   "longname" : "D1 (Na I 5895)",   "lambda" : 5895.92,  "emission": False },
+    {"name" : "Hα",   "longname" : "Balmer α",         "lambda" : 6562.801, "emission": False },
+    ]
+
+def add_lines(fig, z, fig_height=350):
+    line_data = dict()
+    line_data['restwave'] = np.array([row['lambda'] for row in _line_list])
+    line_data['plotwave'] = line_data['restwave'] * (1+z)
+    line_data['name'] = [row['name'] for row in _line_list]
+    line_data['longname'] = [row['name'] for row in _line_list]
+    line_data['plotname'] = [row['name'] for row in _line_list]
+    line_data['emission'] = [row['emission'] for row in _line_list]
+
+    y = list()
+    for i in range(len(line_data['restwave'])):
+        if i == 0:
+            if _line_list[i]['emission']:
+                y.append(fig_height - 50)
+            else:
+                y.append(5)
+        else:
+            if (line_data['restwave'][i] < line_data['restwave'][i-1]+100) and \
+               (line_data['emission'][i] == line_data['emission'][i-1]):
+                if line_data['emission'][i]:
+                    y.append(y[-1] - 15)
+                else:
+                    y.append(y[-1] + 15)
+            else:
+                if line_data['emission'][i]:
+                    y.append(fig_height-50)
+                else:
+                    y.append(5)
+
+    line_data['y'] = y
+
+    #- Add vertical spans to figure
+    lines = list()
+    for w in line_data['plotwave']:
+        s = Span(location=w, dimension='height', line_color='green', line_alpha=0.5)
+        fig.add_layout(s)
+        lines.append(s)
+
+    #- Add labels to figure
+    line_data = bk.ColumnDataSource(line_data)
+    labels = LabelSet(x='plotwave', y='y', x_units='data', y_units='screen',
+                      text='name', text_color='green', text_font_size="8pt",
+                      level='glyph',
+                      x_offset=2, y_offset=0, source=line_data, render_mode='canvas')
+
+    fig.add_layout(labels)
+    return line_data, lines, labels
+
+
 if __name__ == '__main__':
     framefiles = [
         'data/cframe-b0-00000020.fits',
-        'data/cframe-r0-00000020.fits',
-        'data/cframe-z0-00000020.fits',
+        # 'data/cframe-r0-00000020.fits',
+        # 'data/cframe-z0-00000020.fits',
     ]
     plotframes(framefiles)
 
