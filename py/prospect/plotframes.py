560318262603ddcfc825895d10b89ed24cf0185c
armengau <eric.armengaud@cea.fr>
2020-02-21 00:33:19 -0800

    NaNs in thumb gallery

--- py/prospect/plotframes.py
+++ py/prospect/plotframes.py
@@ -306,6 +306,8 @@ def grid_thumbs(spectra, thumb_width, x_range=(3400,10000), thumb_height=None, r
         # Use astropy convolution : handles NaNs
         y_vals = astropy.convolution.convolve(thumb_flux[i_spec,:], kernel)
         y_vals = (y_vals[::resamp_factor])[resamp_factor:-resamp_factor]
+        x_vals = x_vals[~np.isnan(y_vals)] # Needed to avoid 'ValueError: Out of range float values are not JSON compliant'
+        y_vals = y_vals[~np.isnan(y_vals)]
         yampl = np.max(y_vals) - np.min(y_vals)
         ymin = np.min(y_vals) - 0.1*yampl
         ymax = np.max(y_vals) + 0.1*yampl
