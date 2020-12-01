e2e27fd6ec5ffb6071e0edf9224fc8e75384faf1
Benjamin Alan Weaver <baweaver@lbl.gov>
2020-12-01 09:49:04 -0700

    add readthedocs config

--- doc/conf.py
+++ doc/conf.py
@@ -121,7 +121,7 @@ napoleon_include_private_with_doc = True
 # some external dependencies are not met at build time and break the
 # building process.
 autodoc_mock_imports = []
-for missing in ('astropy', 'desiutil'):
+for missing in ('astropy', 'bokeh', 'desispec', 'desitarget', 'jinja2', 'scipy', 'specutils'):
     try:
         foo = import_module(missing)
     except ImportError:
