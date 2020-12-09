2aa3e2937e86cf055f6d3386a79d63a439a27b6b
Benjamin Alan Weaver <baweaver@lbl.gov>
2020-12-09 13:18:11 -0700

    fix missing doc imports

--- doc/conf.py
+++ doc/conf.py
@@ -121,7 +121,8 @@ napoleon_include_private_with_doc = True
 # some external dependencies are not met at build time and break the
 # building process.
 autodoc_mock_imports = []
-for missing in ('astropy', 'bokeh', 'desispec', 'desitarget', 'jinja2', 'scipy', 'specutils'):
+for missing in ('astropy', 'bokeh', 'desiutil', 'desispec', 'desitarget',
+                'jinja2', 'numpy', 'scipy', 'specutils'):
     try:
         foo = import_module(missing)
     except ImportError:
