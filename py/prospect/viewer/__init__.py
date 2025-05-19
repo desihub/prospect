34de74fc6222de6d092df0302c7824db7b070aa9
Benjamin Alan Weaver <benjamin.weaver@noirlab.edu>
2025-05-19 13:05:51 -0700

    undo

--- py/prospect/viewer/__init__.py
+++ py/prospect/viewer/__init__.py
@@ -352,9 +352,9 @@ def plotspectra(spectra, zcatalog=None, redrock_cat=None, notebook=False,
     #-----
     #- Emission and absorption lines
     z = zcatalog['Z'][0] if (zcatalog is not None) else 0.0
-    # viewer_cds.load_spectral_lines(z)
-    # viewer_plots.add_spectral_lines(viewer_cds, figure='main')
-    # viewer_plots.add_spectral_lines(viewer_cds, figure='zoom', label_offset_top=50)
+    viewer_cds.load_spectral_lines(z)
+    viewer_plots.add_spectral_lines(viewer_cds, figure='main')
+    viewer_plots.add_spectral_lines(viewer_cds, figure='zoom', label_offset_top=50)
 
 
     #-------------------------
